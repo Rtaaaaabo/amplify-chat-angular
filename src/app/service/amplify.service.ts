1ff89819bfa75d8a97c696d74dc9a13f20119433
Taku Nakagawa <r.taaaaabo@gmail.com>
2020-07-16 14:22:57 +0900

    Create Service for amplify

--- src/app/service/amplify.service.ts
+++ src/app/service/amplify.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import awsmobile from 'src/aws-exports.js';
+import { Auth } from 'aws-amplify';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AmplifyService {
 
-  constructor() { }
+  _hc;
+
+  constructor() {
+    const client = new AWSAppSyncClient({
+      url: awsmobile.aws_appsync_graphqlEndpoint,
+      region: awsmobile.aws_appsync_region,
+      auth: {
+        type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+        jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
+      }
+    });
+    this._hc = client;
+  }
+
+  hc() {
+    return this._hc.hydrated();
+  }
 }
