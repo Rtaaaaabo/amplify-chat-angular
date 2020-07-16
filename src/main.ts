e07d13a5507ddf4ecd510f855c842269dd051e40
Taku Nakagawa <r.taaaaabo@gmail.com>
2020-07-16 21:42:48 +0900

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import Amplify from 'aws-amplify';
 
 // Amplifyについて
+import Amplify from 'aws-amplify';
 import PubSub from '@aws-amplify/pubsub';
 import API from '@aws-amplify/api';
 import awsmobile from './aws-exports';
 import { Auth } from 'aws-amplify';
+
+Amplify.configure(awsmobile);
+API.configure(awsmobile);
+PubSub.configure(awsmobile);
+Auth.configure(awsmobile);
 
 if (environment.production) {
   enableProdMode();
@@ -17,7 +22,4 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
-Amplify.configure(awsmobile);
-API.configure(awsmobile);
-PubSub.configure(awsmobile);
-Auth.configure(awsmobile);
+
